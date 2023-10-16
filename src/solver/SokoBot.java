c2074008201ed79b3498cfb96ed38bbca3e64c62
Boris Victoria <boris.v.victoria@gmail.com>
2023-10-16 17:40:07 +0800

    I like things that are deep

---
+++ src/solver/SokoBot.java
@@ -0,0 +1,27 @@
+package solver;
+
+public class SokoBot {
+
+  int width;
+  int height;
+  char[][] mapData;
+  char[][] itemsData;
+
+  public SokoBot(int width, int height, char[][] mapData, char[][] itemsData)
+  {
+
+    this.width = width;
+    this.height = height;
+    this.mapData = mapData;
+    this.itemsData = itemsData;
+
+  }
+
+  public String solveSokobanPuzzle()
+  {
+
+
+    return "lrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlrlr";
+  }
+
+}
